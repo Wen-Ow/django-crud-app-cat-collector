b4cfcad15b2367a4fef7596d81e86099124dca62
Wen Ow <ow.wen2022@gmail.com>
2025-05-15 20:54:19 -0400

    setup, define URLs and views, DTL

---
+++ main_app/views.py
@@ -0,0 +1,19 @@
+# main_app/views.py
+
+from django.shortcuts import render
+
+# Import HttpResponse to send text-based responses
+from django.http import HttpResponse
+
+# Define the home view function
+def home(request):
+    # Send a simple HTML response
+    return HttpResponse('<h1>Hello World ᓚᘏᗢ</h1>')
+
+def about(request):
+    # Send a simple HTML response
+    return HttpResponse('<h1>About the CatCollector</h1>')
+
+def about(request):
+    return render(request, 'about.html')
+
