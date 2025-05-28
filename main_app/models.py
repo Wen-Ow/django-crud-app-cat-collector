02fa27430a62cfeeea60618ac200ae065da4c166
Wen Ow <ow.wen2022@gmail.com>
2025-05-27 20:30:39 -0400

    adding class-based views, and one to many relationships

--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,43 @@
 from django.db import models
+from django.urls import reverse
 
+# A tuple of 2-tuples added above our models
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+
+# Cat model
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
-
+    
+    # override string method
     def __str__(self):
         return self.name
     
+    def get_absolute_url(self):
+        return reverse('cat-detail', kwargs={'cat_id': self.id})
+    
+class Feeding(models.Model):
+    date = models.DateField('Feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
 
+    # Create a cat_id column for each feeding in the database
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
 
+    def __str__(self):
+        # Nice method for obtaining the friendly value of a Field.choice
+        return f"{self.get_meal_display()} on {self.date}"
+    
+     # Define the default order of feedings
+    class Meta:
+        ordering = ['-date']  # This line makes the newest feedings appear first
--- main_app/views.py
+++ main_app/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Import HttpResponse to send text-based responses
-from django.http import HttpResponse
+# from django.http import HttpResponse
     # Importing HttpResponse: Notice that we import HttpResponse from django.http. This function is used to construct an HTTP response to send back to the browser, similar to res.send() in Express.
 # bring in our Cat model (from main_app/models.py)
-from django.views.generic.edit import CreateView
+# import create class-based view
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Cat
+from .forms import FeedingForm
 
 # Create your views here.
 
@@ -53,10 +55,41 @@ def cat_index(request):
 
 def cat_detail(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
-    # just returning one cat:
-    return render(request, 'cats/detail.html', {'cat': cat})
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {'cat': cat, 'feeding_form': feeding_form})
 
+# adding a class
 class CatCreate(CreateView):
     model = Cat
     fields = '__all__'
+        # The fields attribute is required and can be used to limit or change the ordering of the attributes from the Cat model are generated in the ModelForm passed to the template.
+        # We've taken advantage of the special '__all__' value to specify that the form should contain all of the Cat Model's attributes. Alternatively, we could have listed the fields in a list like this:
+            # class CatCreate(CreateView):
+                # model = Cat
+                # fields = ['name', 'breed', 'description', 'age']
+        # IOW: different users could have different abilities 
+    # success_url = '/cats/'
+        # commenting this out because apparently it's an outdate way of managing a redirect to a page other than index? we're using get_absolute_url method in the model instead. 
+        # you'll see in CatDelete below, we will use this code because we need the page to redirect back to index since there's no cat entry to redirect to. 
+        
+class CatUpdate(UpdateView):
+    model = Cat
+    # pass specific fields that we want the user to be able to update (compared to '__all__')
+    fields = ['breed', 'description', 'age']
+    
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats/'
+        # redirect back to the index
 
+def add_feeding(request, cat_id):
+    # create a ModelForm instance using the data in request.POST
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # don't save the form to the db until it
+        # has the cat_id assigned
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('cat-detail', cat_id=cat_id)
